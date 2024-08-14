57a91c147f9496b7e0882e5d115ad8de7fa7fd80
hamza24v <107317017+hamza24v@users.noreply.github.com>
2024-08-13 23:55:52 -0400

    readded domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  images: {
+    domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"], // adding this allows you to use your profile images
+  },
 };
 
 export default nextConfig;
